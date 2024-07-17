17b7136b3858f80b53511adc1c3be82edcd4ae95
forest <133188597+Forest815@users.noreply.github.com>
2024-07-17 16:20:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,24 @@ document.querySelectorAll('.copyButton').forEach(button => {
         }, 3000);
     });
 });
+document.querySelectorAll('.copyButton1').forEach(button => {
+    button.addEventListener('click', function() {
+        // ボタンの親要素の中のコード要素を取得
+        var codeElement = button.previousElementSibling;
+        // 一時的なテキストエリアを作成してコードをコピー
+        var textArea = document.createElement('textarea');
+        textArea.value = codeElement.innerText;
+        document.body.appendChild(textArea);
+        textArea.select();
+        document.execCommand('copy');
+        document.body.removeChild(textArea);
+        
+        // ボタンにコピー完了のメッセージを表示
+        button.textContent = 'コピー完了';
+        
+        // 3秒後にボタンのテキストを元に戻す
+        setTimeout(function() {
+            button.textContent = 'コピー';
+        }, 3000);
+    });
+});
